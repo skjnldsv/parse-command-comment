14980d86e8e432af397395d299e243eef7fdbe4c
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-08-03 10:12:07 +0200

    use process env ts
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import * as core from "@actions/core";
-import github from "@actions/github";
 
-try {
-  console.log(github);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run(): Promise<void> {
+  try {
+    console.log(process.env);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
